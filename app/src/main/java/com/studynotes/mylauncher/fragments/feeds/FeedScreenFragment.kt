a7edab597e56c58630446573557c87b8d45bf9ce
TheShivamPatel <shivampatel0700@gmail.com>
2024-10-22 09:16:03 +0530

    News card layout commit

--- app/src/main/java/com/studynotes/mylauncher/fragments/feeds/FeedScreenFragment.kt
+++ app/src/main/java/com/studynotes/mylauncher/fragments/feeds/FeedScreenFragment.kt
@@ -1,60 +1,45 @@
 package com.studynotes.mylauncher.fragments.feeds
 
+import android.graphics.Color
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.studynotes.mylauncher.R
+import com.studynotes.mylauncher.databinding.FragmentFeedScreenBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class FeedScreenFragment : Fragment(R.layout.fragment_feed_screen) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FeedScreenFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FeedScreenFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentFeedScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed_screen, container, false)
+    ): View {
+        binding = FragmentFeedScreenBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FeedScreenFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FeedScreenFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        applyEffectWithBlur()
     }
+
+    private fun applyEffectWithBlur() {
+
+        val gradientDrawable = GradientDrawable(
+            GradientDrawable.Orientation.TOP_BOTTOM,
+            intArrayOf(
+                Color.argb(255, 100, 64, 68),
+                Color.argb(255, 53, 27, 28),
+            )
+        )
+        binding.blurBgLayout.background = gradientDrawable
+    }
+
 }
